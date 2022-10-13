24de4bb71644fcecbb5c7dcad5511247b4720d39
21343060 <112638389+21343060@users.noreply.github.com>
2022-10-13 19:46:01 +0700

    Add files via upload

---
+++ prakk7/src/main/java/com/pbo/prakk7/Tugas2_While.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pbo.prakk7;
+
+/**
+ *
+ * @Created By Nadya Annisa Juventina 21343060
+ */
+public class Tugas2_While {
+      public static void main(String[] args){
+        int i=20;
+
+        do{
+            System.out.println(i);
+            i--;
+        }
+        while(i>=1);
+    } 
+}
---
+++ prakk7/src/main/java/com/pbo/prakk7/While2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pbo.prakk7;
+
+/**
+ *
+ * @Created By Nadya Annisa Juventina 21343060
+ */
+public class While2 {
+    public static void main(String[] args) {
+        int i = 20;
+        
+        while (i < 10)
+        {
+            System.out.println("Pemograman Berorientasi Objek");
+            i++;
+        }    
+        
+    }
+}
